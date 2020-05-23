8eaf062b7aa7b1ccb6704ecf45e3149cf8def978
rostun <rosa.m.tung@gmail.com>
2020-05-23 11:18:56 -0400

    little bit of styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 export default {
 	props: {
-		message: {
+		username: {
 			type: String,
 			required: true
 		}
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,8 +1,11 @@
 <style src="../assets/Dashboard.styl"></style>
 <template>
 	<div class="UserDashboard">
-		<input v-model="message" placeholder="edit me">
-		<p>Message is: {{ message }}</p>
+		<input v-model="username" placeholder="your username">
+		<p>username is: {{ username }}</p>
+
+		<button v-on:click="counter += 1">Add 1</button>
+
 	</div>
 </template>
 <script src ="./Dashboard.js"></script>
