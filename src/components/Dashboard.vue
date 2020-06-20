6ef77876a83d44a3a9bed4164bba2caa24eaded0
rostun <rosa.m.tung@gmail.com>
2020-06-20 10:35:38 -0400

    add pet option

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,24 @@
 <template>
 	<div class="Dashboard full-width">
 		<div class="search">
-			<select v-model="select_input">
-				<option :value="null" disabled>Select Food</option>
-				<option v-for="option in options" v-bind:value="{key: option.key, target: option.target, text: option.text}" :key="option.key">
-					{{ option.text }}
-				</option>
-			</select>
-			<button v-on:click="SubmitFood();">Calculate</button>
+			<div class="search_food">
+				<select v-model="select_input">
+					<option :value="null" disabled>Select Food</option>
+					<option v-for="option in options" v-bind:value="{key: option.key, target: option.target, text: option.text}" :key="option.key">
+						{{ option.text }}
+					</option>
+				</select>
+				<button v-on:click="SubmitFood();">Calculate Food</button>
+			</div>
+			<div class="search_pet">
+				<select v-model="select_input">
+					<option :value="null" disabled>Select Pet</option>
+					<option v-for="option in options" v-bind:value="{key: option.key, target: option.target, text: option.text}" :key="option.key">
+						{{ option.text }}
+					</option>
+				</select>
+				<button v-on:click="SubmitFood();">Calculate Pet</button>
+			</div>
 		</div>
 		<p>your searched for: {{food_input.text}}</p>
 	</div>
